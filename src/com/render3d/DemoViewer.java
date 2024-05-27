5f50f10ea1484ef91f052ba2bf41da8492d5c401
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-05-27 18:41:56 -0300

    feat: create panel with scroll

--- src/com/render3d/DemoViewer.java
+++ src/com/render3d/DemoViewer.java
@@ -1,7 +1,35 @@
 package com.render3d;
 
+
+import javax.swing.*;
+import java.awt.*;
+
 public class DemoViewer {
     public static void main(String[] args) {
-        
+        JFrame frame = new JFrame("My first JFrame");
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        JPanel renderPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0,0, getWidth(), getHeight());
+            }
+        };
+
+        pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400,400);
+        frame.setVisible(true);
     }
 }
+
+
+
