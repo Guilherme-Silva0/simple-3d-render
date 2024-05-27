6d06b2164657fbc5dc9bfecfc57daa2e2ffcc973
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-05-27 19:09:09 -0300

    feat: create Triangle

--- src/com/render3d/DemoViewer.java
+++ src/com/render3d/DemoViewer.java
@@ -43,4 +43,17 @@ class Vertex {
     }
 }
 
+class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+
+    public Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
 
